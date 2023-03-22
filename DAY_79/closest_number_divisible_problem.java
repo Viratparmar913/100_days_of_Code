249a98b17c4c7372276f88db4e176413a4869671
Pankaj  Parmar <115024744+Viratparmar913@users.noreply.github.com>
2023-03-22 10:28:26 +0530

    Create closest_number_divisible_problem.java

---
+++ DAY_79/closest_number_divisible_problem.java
@@ -0,0 +1,79 @@
+/*
+Given non-zero two integers N and M. The problem is to find the number closest to N and divisible by M. 
+If there are more than one such number, then output the one having maximum absolute value.
+
+
+Example 1:
+
+Input:
+N = 13 , M = 4
+Output:
+12
+Explanation:
+12 is the Closest Number to
+13 which is divisible by 4.
+Example 2:
+
+Input:
+N = -15 , M = 6
+Output:
+-18
+Explanation:
+-12 and -18 are both similarly close to
+-15 and divisible by 6. but -18 has
+the maximum absolute value.
+So, Output is -18
+
+
+Your Task:
+You don't need to read input or print anything. 
+Your task is to complete the function closestNumber() which takes an Integer n as input and returns the answer.
+
+
+
+Expected Time Complexity: O(1)
+Expected Auxiliary Space: O(1)
+
+
+
+Constraints:
+-10^5 <= N <= 10^5
+
+*/
+
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String S[] = read.readLine().split(" ");
+
+            int N = Integer.parseInt(S[0]);
+            int M = Integer.parseInt(S[1]);
+
+            Solution ob = new Solution();
+            System.out.println(ob.closestNumber(N,M));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution {
+    static int closestNumber(int N , int M) {
+        // code
+        float q = Math.round(N / (float)M);
+
+    return (int)(q * M);
+    }
+}
